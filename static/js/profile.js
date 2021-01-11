49ae56e091dba3d7d7f6f0e7d496fa96d7a9e9fc
paulloy <paulloy020896@gmail.com>
2021-01-11 22:02:42 +0000

    Assessibility improved. profile.js: buttons that could previously only be clicked can now be selected when focused on and pressing the enter key. profile.html: aria-labels, roles, tabindex added to interactive elements. profile.scss: z-index for ul set to 1, so the ul doesn't display in front of #modal. base.css: .btn-primary background and border colours set to a darker colour for better contrast with white text

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,11 +1,19 @@
-$("#select-pic li").click(function() {
+$("#select-pic li").click(function() {    
     let img = $(this).html();
     $(this).attr("class", "selected");
     $(this).siblings().removeAttr("class");
     document.getElementById("profile-pic").setAttribute("value", img);
     document.getElementById("current-icon").innerHTML = img;
 });
-
+$("#select-pic li").keypress(function(e) {
+    if (e.keyCode === 13) {
+        let img = $(this).html();
+        $(this).attr("class", "selected");
+        $(this).siblings().removeAttr("class");
+        document.getElementById("profile-pic").setAttribute("value", img);
+        document.getElementById("current-icon").innerHTML = img;
+    }
+});
 
 for (let i = 0; i < $("#select-pic").children().length; i++) {
     if ($("#select-pic").children()[i].innerHTML === document.getElementById("profile-pic").value) {
@@ -23,14 +31,41 @@ $("#open-update-profile").click(function() {
     $("#select-pic").show();
     $("#update-password, #delete-account").hide();
 });
+$("#open-update-profile").keypress(function(e) {
+    if (e.keyCode === 13) {
+        $("#modal, #update-profile").css({"display": "flex"});
+        $("#select-pic").show();
+        $("#update-password, #delete-account").hide();
+    }
+});
+
 $("#open-update-password").click(function() {
     $("#modal, #update-password").css({"display": "flex"});
     $("#update-profile, #delete-account").hide();
 });
+$("#open-update-password").keypress(function(e) {
+    if (e.keyCode === 13) {
+        $("#modal, #update-password").css({"display": "flex"});
+        $("#update-profile, #delete-account").hide();
+    }
+});
+
 $("#open-delete-account").click(function() {
     $("#modal, #delete-account").css({"display": "flex"});
     $("#update-password, #update-profile").hide();
 });
+$("#open-delete-account").keypress(function(e) {
+    if (e.keyCode === 13) {
+        $("#modal, #delete-account").css({"display": "flex"});
+        $("#update-password, #update-profile").hide();
+    }
+});
+
 $(".cancel").click(function() {
     $("#modal, #update-profile, #update-password, #delete-account").hide();
+});
+$(".cancel").keypress(function(e) {
+    if (e.keyCode === 13) {
+        $("#modal, #update-profile, #update-password, #delete-account").hide();
+    }
 });
